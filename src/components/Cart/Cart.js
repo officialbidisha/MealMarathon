279dc8fffd7f1a50614d1099c299fc1797160f3d
Bidisha <officialbidisha1@gmail.com>
2021-11-24 09:09:46 +0530

    Completed UI
    1. Added addition and removal logic from cart.
    2. Added an animation for timeout.
    3. For the button clicked on add, used useEffect hook with state to show animation.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,30 +1,56 @@
-import Modal from '../UI/Modal';
-import classes from './Cart.module.css';
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
+
+import CartItem from "./CartItem";
+
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  console.log('total', cartCtx )
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHadler = (id) => {
+    console.log(id);
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({...item, amount:1})
+  };
+
   const cartItems = (
-    <ul className={classes['cart-items']}>
-      {[
-        {
-          id: "c1",
-          name: "Sushi",
-          amount: 2,
-          price: 12.99,
-        },
-      ].map((item) => (
-        <li>{item.name}</li>
+    <ul className={classes["cart-items"]}>
+      {cartCtx.items.map((item) => (
+        <li>
+          {/**
+           * We use bind here in order to preconfigure.
+           */}
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onRemove={cartItemRemoveHadler.bind(null, item.id)}
+            onAdd={cartItemAddHandler.bind(null, item)}
+          ></CartItem>
+        </li>
       ))}
     </ul>
   );
   return (
     <Modal onClose={props.onClose}>
-        {cartItems}
+      {cartItems}
       <div className={classes.total}>
-          <span>Total Amount</span>
-          <span>35.62</span>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-          <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-          <button className={classes.button}>Order</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,53 @@
-import {useContext} from 'react';
-import classes from './HeaderCartButton.module.css';
-import CartIcon from '../Cart/CartIcon';
-import CartContext from '../../store/cart-context';
-const HeaderCartButton = (props) =>{
-    const cartCtx = useContext(CartContext);
-    const numberOfCartItems = cartCtx.items.reduce((curr, item)=> {
-        return curr+item.amount;
-    }, 0);
-    return <button className={classes.button} onClick={props.onClick}>
-        <span className={classes.icon}><CartIcon/></span>
-        <span>Your Cart</span>
-        <span className={classes.badge}>{numberOfCartItems}</span>
+import { useContext, useEffect, useState } from "react";
+import classes from "./HeaderCartButton.module.css";
+import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
+const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  const [buttonIsHighlighted, setButtonIsHighLighted] = useState(false);
+
+  const { items } = cartCtx;
+
+  /**
+   * We want to run this only when items are effected.
+   */
+  useEffect(() => {
+    /**
+     * In case there is no items, this should not happen.
+     */
+    if (items.length === 0) {
+      return;
+    }
+
+    setButtonIsHighLighted(true);
+    const timer = setTimeout(()=> {
+        setButtonIsHighLighted(false);
+    }, 300)
+
+    return () => {
+        clearTimeout(timer);
+    }
+  }, [items]);
+  const numberOfCartItems = cartCtx.items.reduce((curr, item) => {
+    return curr + item.amount;
+  }, 0);
+
+  const buttonClasses = `${classes.button} ${
+    buttonIsHighlighted ? classes.bump : ""
+  }`;
+
+  /**
+   * Animation is a side effect. So use useEffect hook.
+   */
+
+  return (
+    <button className={buttonClasses} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
-}
+  );
+};
 export default HeaderCartButton;
