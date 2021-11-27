cd1b5801d3b21d97a08664844731d8bee55278b6
Bidisha <officialbidisha1@gmail.com>
2021-11-28 01:32:01 +0530

    Added error handling.

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -2,50 +2,30 @@ import classes from "./AvailableMeals.Module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import { useEffect, useState } from "react";
-// const DUMMY_MEALS = [
-//   {
-//     id: "m1",
-//     name: "Sushi",
-//     description: "Finest fish and veggies",
-//     price: 22.99,
-//   },
-//   {
-//     id: "m2",
-//     name: "Schnitzel",
-//     description: "A german specialty!",
-//     price: 16.5,
-//   },
-//   {
-//     id: "m3",
-//     name: "Barbecue Burger",
-//     description: "American, raw, meaty",
-//     price: 12.99,
-//   },
-//   {
-//     id: "m4",
-//     name: "Green Bowl",
-//     description: "Healthy...and green...",
-//     price: 18.99,
-//   },
-// ];
-
 const AvailableMeals = () => {
 
   const [meals,setMeals] = useState([])
+  const [isLoading, setIsLoading] = useState(false);
+  const [httpError, setHttpError] = useState();
   /**
    * The function to useEffect should not return a promise.
    * It must have a clean up function and that needs to be synchronous.
    */
   useEffect(() => {
     const fetchMeals = async () => {
+      setIsLoading(true);
       const response = await fetch(
         "https://react-http-3ebec-default-rtdb.firebaseio.com/meals.json"
       );
+
+      if(!response.ok){
+        throw new Error('Something went wrong!');
+      }
       const responseData = await response.json();
       const loadedMeals = [];
       for( const key in responseData){
         loadedMeals.push({
-          id: key, 
+          id: key,
           name: responseData[key].name,
           description: responseData[key].description,
           price: responseData[key].price
@@ -53,10 +33,35 @@ const AvailableMeals = () => {
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
-    fetchMeals();
+    /**
+     * Error inside a promise will casue it to reject.
+     * To catch this reject we need async await. 
+     * Hence we convert this try-catch.
+     */
+      fetchMeals()
+      .catch((error) => {
+        setIsLoading(false);
+      setHttpError(error.message);
+      });
   }, []);
 
+  if(isLoading){
+    return (
+      <section className={classes.MealIsLoading}>
+        <p>Loading...</p>
+      </section>
+    )
+  }
+
+  if(httpError){
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    )
+  }
   const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
